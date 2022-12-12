ee816149876849a8fe64efd2ddec0b90d7d32aee
Oana Belu <93271350+OanaBelu@users.noreply.github.com>
2022-12-12 17:05:41 +0200

    1
    1

---
+++ day1.py
@@ -0,0 +1,26 @@
+# Basic numbers : mostly integer and float
+# The print function is a built-in function
+# Developing a good coding style is very important
+# Every piece of code we’ve written so far is an expression
+# We can use multiple operators
+# Just like in normal mathematics, we can use parentheses to group operations
+
+"""Exercises
+
+Now that we’ve been through the material for this post, here are a few exercises so that you can practice:
+
+    Print your age to the console. Calculate and print the number of days, weeks, and months in 27 years. Don’t worry
+    about leap years! Calculate and print the area of a circle with a radius of 5 units. You can be as accurate as
+    you like with the value of p """
+
+# 1 - Print your age to the console
+print(30)
+
+# 2 - Calculate and print the number of days, weeks, and months in 27 years
+print("In 27 years there are :", 27 * 12, "months")
+print("In 27 years there are :", 27 * 52, "weeks")
+print("In 27 years there are :", 27 * 365, "days")
+
+# 3 - Calculate and print the area of a circle with a radius of 5 units. You can be as accurate as you like with the
+# value of pi
+print("Aria of a cicle with radius 5 is :", 5 ** 2 * 3.1415)
