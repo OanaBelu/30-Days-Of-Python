29b739a9f0b8914625c67a4f10d649a34f8df974
Oana Belu <93271350+OanaBelu@users.noreply.github.com>
2022-12-13 15:19:43 +0200

    a

--- day6.py
+++ day6.py
@@ -27,7 +27,6 @@ prints out only those who have an hourly wage above the calculated average."""
 # 1) Below we've provided a list of tuples, where each tuple contains details about an employee of a shop: their name,
 # the number of hours worked last week, and their hourly rate. Print how much each employee is due to be paid at the end
 # of the week in a nice, readable format.
-
 employees = [
     ("Rolf Smith", 35, 8.75),
     ("Anne Pun", 30, 12.50),
@@ -37,3 +36,20 @@ employees = [
 for employee in employees:
     total_pay = employee[1] * employee[2]
     print(f'{employee[0]} has earn last week {total_pay}')
+
+# 2) For the employees above, print out those who are earning an hourly wage above average.
+
+media = 0
+x = 0
+for employee in employees:
+    media = media + employee[2]
+    x +=1
+
+average = media/x
+print(average)
+
+for employee in employees:
+    if average < employee[2]:
+        print(f'{employee[0]} earns more than {average}, which is the average.')
+
+
