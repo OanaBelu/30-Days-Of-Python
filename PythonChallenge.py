be2d4023f17dfd9b6c59ddf5897e34e1a8a018a8
Oana Belu <93271350+OanaBelu@users.noreply.github.com>
2022-12-15 14:14:06 +0200

    Project Python Challenge

---
+++ PythonChallenge.py
@@ -0,0 +1,24 @@
+
+# Incepem astazi quiz-ul.
+
+# Python Challenge day 1:
+lst_1 = [1, 2, 3, 4]
+lst_2 = [1, 2, 3, 4]
+print(lst_1 == lst_2, lst_1 is lst_2) # True False
+# True True
+# True False
+# False True
+# False False
+# De ce ai facut aceasta alegere?
+
+# Python Challenge day 2 :
+# Se da variabila numere, care este o lista si variabila suma, care reprezinta un intreg
+# Returneaza indicii oricaror doua numere din lista numere, care adunate, vor fi egale cu valoarea variabilei suma
+# Ex:
+numere = [1, 4, 7, 9]
+suma = 5
+# output: 0,1 pentru ca numere[0] + numere[1] = 1 + 4 = 5
+#
+# numere = [5, 6, 3 ,7]
+# suma = 10
+# output: 2,3
