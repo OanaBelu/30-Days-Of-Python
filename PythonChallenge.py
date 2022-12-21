4c1639f2f9b1e0f56a5f01388a4f59d17e421dab
Oana Belu <93271350+OanaBelu@users.noreply.github.com>
2022-12-21 15:36:30 +0200

    Exercises

--- PythonChallenge.py
+++ PythonChallenge.py
@@ -22,3 +22,101 @@ suma = 5
 # numere = [5, 6, 3 ,7]
 # suma = 10
 # output: 2,3
+
+
+
+for _ in range (2,6):
+    print("cdr")
+
+friends =[ "Ana"" Rud "" Cld"]
+print(",".join(friends))
+
+while True:
+    num = int(input("Number : "))
+
+    if num < 100:
+        break
+names = ["ana", "fer", "kdfr"]
+
+for counter, name in enumerate(names):
+    print((f'{counter}. {name}'))
+
+names = ["ana", "fer", "kdfr"]
+pets = ["caine", "pisica", "cal"]
+for name, pet in zip(names,pets):
+    print((f'{name.title()} owns {pet}'))
+
+# The repr() function returns a printable representational string of the given object.
+
+def generate_prompt(animal):
+
+    return """Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: {}
+Names:""".format(animal.capitalize())
+
+generate_prompt("Cat")
+
+# Normal function
+def normal_test():
+    return "Hello World"
+
+
+# Generator function
+def generator_test():
+    yield "Hello World"
+
+
+print(normal_test())  # call to normal function
+print(generator_test())  # call to generator function
+
+print(normal_test())
+print(next(generator_test()))  # will output Hello World
+print(list(generator_test()))
+
+
+def even_numbers(n):
+    for x in range(n):
+        if (x % 2 == 0):
+            yield x
+
+
+num = even_numbers(10)
+print(list(num))
+
+
+def even_numbers(n):
+    for x in range(n):
+        if (x % 2 == 0):
+            yield x
+
+
+num = even_numbers(10)
+print(next(num))
+print(next(num))
+print(next(num))
+print(next(num))
+print(next(num))
+
+
+# print(next(num))
+
+def getFibonnaciSeries(num):
+    c1, c2 = 0, 1
+    count = 0
+    while count < num:
+        yield c1
+        c3 = c1 + c2
+        c1 = c2
+        c2 = c3
+        count += 1
+
+
+fin = getFibonnaciSeries(7)
+print(list(fin))
+for i in fin:
+    print(i)
