f6bc8caaf77edcdbe0990e051f713f6b0dc6bb8c
Oana Belu <93271350+OanaBelu@users.noreply.github.com>
2023-01-04 12:23:47 +0200

    Day 30

--- day30.py
+++ day30.py
@@ -8,3 +8,26 @@ Today's project is particularly large so there are two project posts for today.
 
 The first is going to be a project preparation post which will walk you through everything you need to know about
 pygame. The second will contain a walkthrough for the entire project, and will also contain the project brief."""
+
+import pygame
+import pygame.display
+
+WINDOW_HEIGHT = 840
+WINDOW_WIDTH = 800
+WINDOW_DIMENSIONS = WINDOW_WIDTH, WINDOW_HEIGHT
+
+pygame.init()
+pygame.display.set_caption("Snake")
+
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode(WINDOW_DIMENSIONS)
+
+def play_game():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        clock.tick(30)
+
+play_game()
