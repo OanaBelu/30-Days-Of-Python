f89e07b91845c47896420a3ce9c33c56eb5b0f33
Oana Belu <93271350+OanaBelu@users.noreply.github.com>
2023-01-04 12:29:45 +0200

    Project Plotting Graphs

--- ProjectPlottingGraphs.py
+++ ProjectPlottingGraphs.py
@@ -38,3 +38,52 @@ I would recommend tackling this project this way:
     Create a file, such as data_storage.py, that contains functions to read the iris.csv data file.
     Create a third file, graphing.py, that contains a function that creates the scatter plot given the x and y values.
 """
+
+# from matplotlib import pyplot
+#
+# x_data = [1, 2, 3, 4, 5]
+# y_data = [5.5, 6.4, 5.3, 4.4, 7.9]
+#
+# figure=pyplot.figure()
+# pyplot.scatter(x_data, y_data)
+# pyplot.savefig("graph.png")
+#
+# x_data = [1, 2, 3, 4, 5]
+# y_data = [1.5, 3.4, 4.2, 2.4, 5.1]
+#
+# figure=pyplot.figure()
+# pyplot.scatter(x_data, y_data)
+# pyplot.savefig("graph.png")
+
+from graphing import create_chart
+from data_storage import read_column
+
+user_menu = """Please choose from the following options:
+
+- Enter 'c' to chart a new graph.
+- Enter 'q' to quit.
+
+Your selection: """
+
+charting_menu = "Enter the column you'd like to chart: "
+filename_prompt = "Enter your desired file name: "
+
+
+def handle_chart():
+    column = int(input(charting_menu))
+    x = read_column(-1)
+    y = [float(n) for n in read_column(column)]
+
+    filename = input(filename_prompt)
+    create_chart(x, y, filename.strip())
+
+
+while True:
+    user_selection = input(user_menu)
+    if user_selection == "q":
+        break
+    elif user_selection == "c":
+        handle_chart()
+    else:
+        print(f"Sorry, '{user_selection}' is not a valid option.")
+
